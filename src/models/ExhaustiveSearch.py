3725cce9de598dae2e77d249a1f7b1012ee198a7
Raoef Taki <r.taki@student.tue.nl>
2022-07-21 00:57:55 +0200

    Bug fix

--- src/models/ExhaustiveSearch.py
+++ src/models/ExhaustiveSearch.py
@@ -135,11 +135,11 @@ class ExhaustiveSearch(nn.Module):
                 resulting_model_idx = res[2]
                 models[resulting_model_idx] = resulting_model
 
-        raise ValueError(n_ep_max_original, all_res)
+        # raise ValueError(n_ep_max_original, all_res)
         # TODO, test: Does it report good outcomes for 'all_res', or do you somehow need to pass this through in all loops?
         # TODO: may take longer because of the multiple processpoolexecutors
         for path, res in zip(self.models_idx.keys(), all_res):
-            self.res[path] = res
+            self.res[path] = res[0]
 
         all_res = list(map(lambda x: (x[1][2]['value'], x[0]), self.res.items()))
         best_path = max(all_res, key=itemgetter(0))[1]
