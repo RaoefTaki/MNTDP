69ee7c5b39ed8921e4b43cddb6b5a56ac361ee6d
Raoef Taki <r.taki@student.tue.nl>
2022-07-27 13:49:00 +0200

    Try print model to see growth

--- src/experiments/stream_tuning.py
+++ src/experiments/stream_tuning.py
@@ -581,7 +581,8 @@ def train_single_task(t_id, task, tasks, vis_p, learner, config, transfer_matrix
     # optim_func=functools.partial(<class 'torch.optim.adam.Adam'>,
     # weight_decay=0, lr=0.001, betas=[0.9, 0.999]),
     # optim_params=[{'architecture': 5, 'lr': 0.01, 'weight_decay': 0}], split_optims=True))
-    raise ValueError("model:", model)
+    # TODO: check to see if the model actually grows over time
+    raise ValueError("model.models:", model.models, "model.models_idx:", model.models_idx, "model.get_graph():", model.get_graph())
 
     loss_fn = task['loss_fn']
     training_params['loss_fn'] = loss_fn
