8b1d259c762fc1f3e7937b3e8eac0b4089999619
Raoef Taki <r.taki@student.tue.nl>
2022-08-19 21:19:10 +0200

    Print types in knn

--- src/models/PSSN_llmodel.py
+++ src/models/PSSN_llmodel.py
@@ -201,6 +201,7 @@ class MNTDP(LifelongLearningModel, ModularModel):
             for x, y in train_dataset:
                 x = x.to(device)
                 y = y.to(device)
+                raise ValueError(type(x), x, type(y), y)
                 feats, preds = pruned_model.feats_forward(x)
                 features.append(flatten(feats))
                 labels.append(y)
