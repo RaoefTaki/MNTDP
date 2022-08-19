8d7dc52fc7b220c1cc67d249114cdba143460f5b
Raoef Taki <r.taki@student.tue.nl>
2022-08-19 21:23:36 +0200

    Print more info

--- src/models/PSSN_llmodel.py
+++ src/models/PSSN_llmodel.py
@@ -201,7 +201,7 @@ class MNTDP(LifelongLearningModel, ModularModel):
             for x, y in train_dataset:
                 x = x.to(device)
                 y = y.to(device)
-                raise ValueError(type(x), x, type(y), y)
+                raise ValueError(type(x), x.shape(), x, type(y), y.shape(), y)
                 feats, preds = pruned_model.feats_forward(x)
                 features.append(flatten(feats))
                 labels.append(y)
