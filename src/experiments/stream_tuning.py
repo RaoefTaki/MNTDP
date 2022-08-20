0092b0d64aff820ce5bc3630589a0130cb798f99
Raoef Taki <r.taki@student.tue.nl>
2022-08-20 23:51:05 +0200

    Print more

--- src/experiments/stream_tuning.py
+++ src/experiments/stream_tuning.py
@@ -464,12 +464,6 @@ def train_on_tasks(config):
             print("[TEST] best_trial's arch_scores:", best_trial.last_result["arch_scores"])  # self.arch_scores[task_id]['knn']
             print("[TEST] Finished learning on task:", t_id)
 
-            print(learner.fixed_graphs[t_id])
-            for path_generated in nx.all_simple_paths(learner.fixed_graphs[t_id], (t_id, learner.IN_NODE), (t_id, learner.OUT_NODE)):
-                print(path_generated)
-            print(nx.all_simple_paths(learner.fixed_graphs[t_id], (t_id, learner.IN_NODE), (t_id, learner.OUT_NODE)))
-            exit(0)
-
             # Backward transfer
             print("[TEST] Trying for backward transfer now based on task:", t_id)
             knn_accuracies, tasks_bw_output_head = check_possibility_backward_transfer(memory_buffer=memory_buffer,
@@ -508,6 +502,15 @@ def train_on_tasks(config):
     print("***")
     print(learner.columns)
     print("***")
+    for t_id, (task, vis_p) in enumerate(zip(tasks, task_vis_params)):
+        print("Path generation for t_id:", t_id)
+        for path_generated in nx.all_simple_paths(learner.fixed_graphs[t_id], (t_id, learner.IN_NODE), (t_id, learner.OUT_NODE)):
+            print(path_generated)
+    print("***")
+    print("Test:")
+    print(nx.all_simple_paths(learner.fixed_graphs[0], (0, learner.IN_NODE), (0, learner.OUT_NODE))[0])
+    print("***")
+
     # Edited example of outcome of print(learner.columns), figure out how to loop over this:
     # [{0: {(0, 0): '(0, 0)'}, "INs": {(0, "INs"): '(0, "INs")', (0, "INs", 0): '(0, "INs", 0)'}, 1: {(0, 1, "w"): '(0, 1, "w")', (0, 1): '(0, 1)'}, 2: {(0, 2, "w"): '(0, 2, "w")', (0, 2): '(0, 2)'}, 3: {(0, 3, "w"): '(0, 3, "w")', (0, 3): '(0, 3)'}, 4: {(0, 4, "w"): '(0, 4, "w")', (0, 4): '(0, 4)'}, 5: {(0, 5, "w"): '(0, 5, "w")', (0, 5): '(0, 5)'}, 6: {(0, 6, "w"): '(0, 6, "w")', (0, 6): '(0, 6)'}, "OUT": {(0, "OUT"): '(0, "OUT")', (0, "OUT", 0): '(0, "OUT", 0)'}}, {0: {}, "INs": {(1, "INs"): '(1, "INs")', (1, "INs", 0): '(1, "INs", 0)'}, 1: {}, 2: {}, 3: {}, 4: {(1, 4): '(1, 4)', (1, 4, 0, "f"): '(1, 4, 0, "f")'}, 5: {(1, 5, "w"): '(1, 5, "w")', (1, 5): '(1, 5)'}, 6: {(1, 6, "w"): '(1, 6, "w")', (1, 6): '(1, 6)'}, "OUT": {(1, "OUT"): '(1, "OUT")', (1, "OUT", 1): '(1, "OUT", 1)'}}, {0: {}, "INs": {(2, "INs"): '(2, "INs")', (2, "INs", 0): '(2, "INs", 0)'}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {(2, 5): '(2, 5)', (2, 5, 1, "f"): '(2, 5, 1, "f")'}, 6: {(2, 6, "w"): '(2, 6, "w")', (2, 6): '(2, 6)'}, "OUT": {(2, "OUT"): '(2, "OUT")', (2, "OUT", 2): '(2, "OUT", 2)'}}, {0: {(3, 0): '(3, 0)'}, "INs": {(3, "INs"): '(3, "INs")', (3, "INs", 3): '(3, "INs", 3)'}, 1: {(3, 1, "w"): '(3, 1, "w")', (3, 1): '(3, 1)'}, 2: {(3, 2, "w"): '(3, 2, "w")', (3, 2): '(3, 2)'}, 3: {(3, 3, "w"): '(3, 3, "w")', (3, 3): '(3, 3)'}, 4: {(3, 4, "w"): '(3, 4, "w")', (3, 4): '(3, 4)'}, 5: {(3, 5, "w"): '(3, 5, "w")', (3, 5): '(3, 5)'}, 6: {(3, 6, "w"): '(3, 6, "w")', (3, 6): '(3, 6)'}, "OUT": {(3, "OUT"): '(3, "OUT")', (3, "OUT", 3): '(3, "OUT", 3)'}}]
     for key, value in tasks_bw_output_head.items():
