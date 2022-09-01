315bf3fb77e1b9ef4372eb3113358bf39588424f
Raoef Taki <r.taki@student.tue.nl>
2022-09-01 04:08:07 +0200

    Try fix

--- src/experiments/stream_tuning.py
+++ src/experiments/stream_tuning.py
@@ -482,7 +482,8 @@ def train_on_tasks(config):
 
             # Calculate how many parameters are saved by performing BWTr this iteration
             _, _, _, _, nr_of_parameters_saved_list = \
-                get_bw_transfer_info()
+                get_bw_transfer_info(tasks=tasks, last_task_id=t_id, task_vis_params=task_vis_params, config=config,
+                                     learner=learner, tasks_list=tasks_list, tasks_bw_output_head=tasks_bw_output_head)
             nr_MB_saved_due_to_bwtr = ((sum(nr_of_parameters_saved_list)*32)/8)/1000000
 
             # Calculate the extra memory requirements available to use for the memory buffer obtained in this iteration
@@ -537,7 +538,8 @@ def train_on_tasks(config):
     # Edited example of outcome of print(learner.columns), figure out how to loop over this:
     # [{0: {(0, 0): '(0, 0)'}, "INs": {(0, "INs"): '(0, "INs")', (0, "INs", 0): '(0, "INs", 0)'}, 1: {(0, 1, "w"): '(0, 1, "w")', (0, 1): '(0, 1)'}, 2: {(0, 2, "w"): '(0, 2, "w")', (0, 2): '(0, 2)'}, 3: {(0, 3, "w"): '(0, 3, "w")', (0, 3): '(0, 3)'}, 4: {(0, 4, "w"): '(0, 4, "w")', (0, 4): '(0, 4)'}, 5: {(0, 5, "w"): '(0, 5, "w")', (0, 5): '(0, 5)'}, 6: {(0, 6, "w"): '(0, 6, "w")', (0, 6): '(0, 6)'}, "OUT": {(0, "OUT"): '(0, "OUT")', (0, "OUT", 0): '(0, "OUT", 0)'}}, {0: {}, "INs": {(1, "INs"): '(1, "INs")', (1, "INs", 0): '(1, "INs", 0)'}, 1: {}, 2: {}, 3: {}, 4: {(1, 4): '(1, 4)', (1, 4, 0, "f"): '(1, 4, 0, "f")'}, 5: {(1, 5, "w"): '(1, 5, "w")', (1, 5): '(1, 5)'}, 6: {(1, 6, "w"): '(1, 6, "w")', (1, 6): '(1, 6)'}, "OUT": {(1, "OUT"): '(1, "OUT")', (1, "OUT", 1): '(1, "OUT", 1)'}}, {0: {}, "INs": {(2, "INs"): '(2, "INs")', (2, "INs", 0): '(2, "INs", 0)'}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {(2, 5): '(2, 5)', (2, 5, 1, "f"): '(2, 5, 1, "f")'}, 6: {(2, 6, "w"): '(2, 6, "w")', (2, 6): '(2, 6)'}, "OUT": {(2, "OUT"): '(2, "OUT")', (2, "OUT", 2): '(2, "OUT", 2)'}}, {0: {(3, 0): '(3, 0)'}, "INs": {(3, "INs"): '(3, "INs")', (3, "INs", 3): '(3, "INs", 3)'}, 1: {(3, 1, "w"): '(3, 1, "w")', (3, 1): '(3, 1)'}, 2: {(3, 2, "w"): '(3, 2, "w")', (3, 2): '(3, 2)'}, 3: {(3, 3, "w"): '(3, 3, "w")', (3, 3): '(3, 3)'}, 4: {(3, 4, "w"): '(3, 4, "w")', (3, 4): '(3, 4)'}, 5: {(3, 5, "w"): '(3, 5, "w")', (3, 5): '(3, 5)'}, 6: {(3, 6, "w"): '(3, 6, "w")', (3, 6): '(3, 6)'}, "OUT": {(3, "OUT"): '(3, "OUT")', (3, "OUT", 3): '(3, "OUT", 3)'}}]
     keys_list, values_list, old_evaluation_accuracy_list, new_evaluation_accuracy_list, nr_of_parameters_saved_list =\
-        get_bw_transfer_info()
+        get_bw_transfer_info(tasks=tasks, last_task_id=None, task_vis_params=task_vis_params, config=config,
+                             learner=learner, tasks_list=tasks_list, tasks_bw_output_head=tasks_bw_output_head)
     for key_id in range(len(keys_list)):
         print("[RESULT] Updated task", keys_list[key_id], "using the model of task", values_list[key_id]['other_t_id'],
               "to update the evaluation accuracy from", old_evaluation_accuracy_list[key_id], "to", new_evaluation_accuracy_list[key_id])
